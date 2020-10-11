940e43c83be872a5656b7f1dd369a202ef460fc4
siwenhongyi <zmlhongyi@vip.qq.com>
2020-10-11 22:59:14 +0800

    add index.html home.html

--- Picts/views.py
+++ Picts/views.py
@@ -1,3 +1,17 @@
+import datetime
+
 from django.shortcuts import render
+# my packages
+feom datetime im
 
 # Create your views here.
+def index(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'Picts/index.html',
+        {
+            'title': 'Home',
+            'year': datetime.now().year,
+        }
+    )
