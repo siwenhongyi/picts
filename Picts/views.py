94d9f4d678b9a82eb4b726cd398882f3bd835700
xudeao <2225691756@qq.com>
2020-10-12 18:19:10 +0800

    ci

--- Picts/views.py
+++ Picts/views.py
@@ -1,7 +1,20 @@
 import datetime
 
 from django.shortcuts import render
+from django.http import  HttpResponse
 # my packages
 
 
 # Create your views here.
+def index(request ):
+    return render(
+        request,
+        "index.html"
+    )
+
+
+def user(request):
+    return render(
+        request,
+        "user.html"
+    )
