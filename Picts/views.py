829ea75c593377bb0f721f9e3f37d804675ddbe6
siwenhongyi <zmlhongyi@vip.qq.com>
2020-10-15 19:20:09 +0800

    ci

--- Picts/views.py
+++ Picts/views.py
@@ -11,32 +11,32 @@ import json
 
 # Create your views here.
 from App import settings
-from Picts.models import Pict,Kind
+from Picts.models import Pict, Kind
 
 path = '/Picts/static/background'
 
 
 def index(request):
-    f1 = os.listdir(os.getcwd()+path)
-    kind_n =  Kind.objects.all().count()
+    f1 = os.listdir(os.getcwd() + path)
+    kind_n = Kind.objects.all().count()
     kinds = Kind.objects.all()
-    index =1
+    index = 1
     for i in f1:
-        ran1 = random.randint(2,kind_n+1)
+        ran1 = random.randint(2, 3)
         model = Pict()
         model.pict_id = str(index)
         model.pic_url = i
         kind_need = []
         model.save()
-        for k in range(ran1):
-            ran2 = random.randint(0, kind_n-1)
+        while len(kind_need) != ran1:
+            ran2 = random.randint(0, kind_n - 1)
             while kinds[ran2] in kind_need:
-                ran2 = random.randint(0, kind_n-1)
+                ran2 = random.randint(0, kind_n - 1)
             model.kind.add(kinds[ran2])
             kind_need.append(kinds[ran2])
-        index+=1
+        index += 1
     return HttpResponse('图片上传成功')
-    f = os.listdir(os.getcwd()+path)
+    f = os.listdir(os.getcwd() + path)
     cnt = len(f)
     x = random.randint(0, cnt)
     name = 'LOL_122.jpg'
@@ -52,26 +52,34 @@ def index(request):
             'background_img_name': json.dumps([name]),
         }
     )
+
+
 def person(request):
     return render(
         request,
         "person.html",
     )
+
+
 def user(request):
     return render(
         request,
         "user.html",
     )
+
+
 def photo(request):
     search_photos = Pict()
-    kind = Kind.objects.filter(kind_name = request.POST.get("information"))
+    kind = Kind.objects.filter(kind_name=request.POST.get("information"))
     print(kind)
     search_photos = Pict.objects.filter(kind=kind)
-    return render(request,'photo.html',
+    return render(request, 'photo.html',
                   {
-                      'photos':search_photos
+                      'photos': search_photos
                   }
-    )
+                  )
+
+
 def up_img(request):
     pic = request.FILES.get('picture')
     model = Pict()
@@ -80,4 +88,3 @@ def up_img(request):
     print(model.pic_url)
     model.save()
     return HttpResponse('图片上传成功')
-
